de11e68d4884f086635c12aed835277b531393a7
QCA-SDC\ywang <ywang@aquent.com>
2017-12-29 14:16:36 -0600

    refactor: change reducer from using switch to lookup table

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,31 @@
 import constants from '../../src/actions/constants'
 
-const reducer = (state = {menu: [], display: []}, action) => {
-    switch(action.type) {
-        case constants.GET_PIZZA:
-            return { 
-                menu: action.data, 
-                display: action.data
-            }
-        case constants.FILTER_PIZZA:
-            return { 
-                menu: state.menu, 
-                display: state.menu.filter(item => (
-                        item.toLowerCase().includes(action.data.toLowerCase())
-                ))
-            }
-        case constants.SORT_PIZZA:
-            return {
-                menu: state.menu, 
-                display: [...state.display].sort((a, b) => a.toLowerCase() < b.toLowerCase() ? 1: -1)
-            }
- 
-        default:
-            return state
+const getPizza = (state, action) => ({ 
+    menu: action.data, 
+    display: action.data
+})
+
+const filterPizza = (state, action) => ({
+    menu: state.menu, 
+    display: state.menu.filter(item => (
+            item.toLowerCase().includes(action.data.toLowerCase())
+    ))
+})
+
+const sortPizza = (state, action) => ({
+    menu: state.menu, 
+    display: [...state.display].sort((a, b) => a.toLowerCase() < b.toLowerCase() ? 1: -1)
+})
+
+const rootReducer = (state = {menu: [], display: []}, action) => {
+    const actionTypes = {
+        [constants.GET_PIZZA]: getPizza,
+        [constants.FILTER_PIZZA]: filterPizza,
+        [constants.SORT_PIZZA]: sortPizza 
     }
+
+    const reducer = actionTypes[action.type]
+    return reducer ? reducer(state, action) : state 
 }
 
-export default reducer
+export default rootReducer
